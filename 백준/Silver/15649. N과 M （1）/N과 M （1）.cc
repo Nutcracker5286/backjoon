d7c62a9f2dc099dd9dd9caf99fc7b1dde963cbcc
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2024-12-23 15:02:41 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,35 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-bool flag[10];
+int m,n;
 int arr[10];
-void solve(int t){
-    if(t==m){
-        for (int i = 0; i < m; i++)
-        {
-            cout<<arr[i]<<' ';
-        }
-        cout<<'\n';
-        return;
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if(!flag[i]){
-            arr[t]=i;
-            flag[i]=true;
-            solve(t+1);
-            flag[i]=false;
+bool isUsed[10];
 
+void solve(int k){
+    if(k==m){
+        for(int i=0; i<m; i++)
+            cout<<arr[i]<<' ';
+        cout<<'\n';
+        return ;
+    }
+
+    for (int  i = 1; i <= n; i++)
+    {
+        if(!isUsed[i]){
+            arr[k]=i;
+            isUsed[i]=1;
+            solve(k+1);
+            isUsed[i]=0;
         }
     }
     
 }
 
 int main(){
-    ios_base::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
     cin.tie(0);
-
     cin>>n>>m;
+
     solve(0);
 }
