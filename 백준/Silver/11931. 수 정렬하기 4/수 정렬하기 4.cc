95693465da9abe99e406bdbe9e94950972ce5442
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-02-12 21:29:28 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 276 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -1,41 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arr[1000005];
-int tmp[1000005];
-int n;
 
-void merge(int st, int en){
-  int mid=(st+en)/2;
+bool cmp(int a, int b){return a>b;}
 
-  int leftIdx=st;
-  int rightIdx=mid;
-  for(int i=st; i<en; i++){
-    if(leftIdx==mid) tmp[i]=arr[rightIdx++];
-    else if(rightIdx==en) tmp[i]=arr[leftIdx++];
-    else if(arr[leftIdx]>=arr[rightIdx]) tmp[i]=arr[leftIdx++];
-    else tmp[i]=arr[rightIdx++];
-  }
-  for(int i=st; i<en; i++) arr[i]=tmp[i];
-}
-
-
-void merge_sort(int st, int en){
-  if(en==st+1) return;
-  int mid=(st+en)/2;
-
-  merge_sort(st,mid);
-  merge_sort(mid,en);
-  merge(st,en);
-}
-
-int main(){
+int arr[10000005];
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  cin>>n;
-  for(int i=0; i<n; i++) cin>>arr[i];
-
-  merge_sort(0,n);
-  for(int i=0; i<n; i++) cout<<arr[i]<<'\n';
+    int n;
+  cin >> n;
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  sort(arr,arr+n, cmp);
+    
+    for(int i = 0; i < n; i++){
+    cout<<arr[i]<<'\n';
+  }
 }
