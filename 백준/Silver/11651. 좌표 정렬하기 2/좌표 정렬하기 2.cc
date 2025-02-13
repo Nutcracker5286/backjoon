5cda0c60d5014c4377cfb5296c04f2a70b80a2de
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-02-13 16:14:06 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 52 ms, Memory: 3200 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool cmp1(pair<int,int> a, pair<int,int> b){
+  return a.second < b.second;
+}
+
+
+bool cmp2(pair<int,int> a, pair<int,int> b){
+  return a.first < b.first;
+}
+
+int n;
+pair<int, int> co[100005];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin>>n;
+
+  for(int i=0; i<n; i++){
+    cin>>co[i].first>>co[i].second;
+  }
+
+  sort(co,co+n,cmp2);
+  stable_sort(co, co+n,cmp1);
+
+
+  for(int i=0; i<n; i++){
+   cout<<co[i].first<<' '<<co[i].second<<'\n';
+  }
+}
