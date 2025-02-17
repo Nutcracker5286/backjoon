8802a032074bbd337202a095c7f27edcb2c82b09
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-02-17 14:10:44 +0900

    [Silver IV] Title: 카드, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.cc
+++ 백준/Silver/11652. 카드/카드.cc
@@ -1,42 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+int n;
+ll arr[100005];
 
-
-long long arr[100005];
-int cnt[100005];
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
-  int ans=0;
-
   cin>>n;
-  
-  for(int i=0; i<n; i++){
-    cin>>arr[i];
-  }
+
+  for(int i=0; i<n; i++)
+  cin>>arr[i];
+
   sort(arr,arr+n);
 
-  long long maxNum=arr[0];
-  int maxCnt=1;
+
   int curCnt=1;
-
-  for(int i = 1; i < n; i++){
-    if(arr[i] == arr[i-1]){
+  int maxCnt=1;
+  ll maxNum=arr[0];
+  for(int i=1; i<n; i++){
+    if(arr[i-1]==arr[i]){
         curCnt++;
-    } else {
-        curCnt = 1;
     }
-    
-    // 현재 카운트가 최대 카운트보다 크거나
-    // 카운트가 같은데 현재 숫자가 더 작은 경우 업데이트
-    if(curCnt > maxCnt || (curCnt == maxCnt && arr[i] < maxNum)){
-        maxCnt = curCnt;
-        maxNum = arr[i];
+    else{
+      curCnt=1;
     }
-}
 
-cout << maxNum;
-return 0;
+    if(curCnt> maxCnt || (curCnt==maxCnt&& arr[i]<= maxNum)){
+      maxCnt=curCnt;
+      maxNum=arr[i];
+    }
+  }
+
+  cout<<maxNum;
 }
