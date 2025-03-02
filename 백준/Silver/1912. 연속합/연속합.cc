e94c542df2e79f8fd09aacc6ce6287b40cac5b10
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-02 11:45:14 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n;
+int arr[100005];
+int d[100005];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin>>n;
+
+  for(int i=1; i<=n; i++)
+  cin>>arr[i];
+
+  for(int i=1; i<=n; i++){
+    d[i]=max(0,d[i-1])+arr[i];
+  }
+
+  cout<<*max_element(d+1,d+1+n);
+}
