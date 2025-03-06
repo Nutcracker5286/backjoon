c0c74506b673b4335640544da55353cd62022cf8
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-06 19:04:30 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -6,22 +6,24 @@ int p[20];
 int t[20];
 int d[20];
 
-
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    cin>>n;
 
-    for(int i=1; i<=n; i++)
-    cin>>t[i]>>p[i];
+  cin>>n;
+  for(int i=1; i<=n; i++)
+  cin>>t[i]>>p[i];
 
-    for(int i=n; i>=1; i--){
-        if(i+t[i]<=n+1){
-            d[i]=max(d[i+t[i]]+p[i],d[i+1]);
-        }
-        else
-            d[i]=d[i+1];
+
+  for(int i=n; i>=1; i--){
+      if(i+t[i]<=n+1){
+        d[i]=max(d[i+1],d[i+t[i]]+p[i]);
+      }
+      else{
+        d[i]=d[i+1];
+      }
     }
-    cout<<*max_element(d+1,d+1+n);
+  cout<<*max_element(d+1,d+1+n);
+
 }
