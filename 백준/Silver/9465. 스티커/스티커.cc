11cb9e4fbb8c79d277791710c1df23d15e0df39b
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-15 00:20:31 +0900

    [Silver I] Title: 스티커, Time: 76 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,37 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
+int n;
+int a[3][100005];
+int d[3][100005];
+
+
+int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int tc;
-  cin >> tc;
-  while (tc--) {
-    int N;
-    cin >> N;
-    vector<vector<int>> arr(N, vector<int>(2)), dp(N, vector<int>(2)); // N by 2 vector를 선언
-    for (int j = 0; j < 2; j++)
-      for (int i = 0; i < N; i++)
-        cin >> arr[i][j];
-    
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < 2; j++) {
-        int v = 0;
-        if (i > 1) v = max(dp[i - 2][0], dp[i - 2][1]);
-        if (i > 0) v = max(v, dp[i - 1][1 - j]);
-        dp[i][j] = v + arr[i][j];
+  int t;
+  cin>>t;
+
+  while (t--)
+  {
+
+   
+    cin>>n;
+
+    for(int i=1; i<=2; i++){
+      for(int j=1; j<=n; j++){
+        a[i][j]=d[i][j]=0;
       }
     }
-    cout << max(dp[N - 1][0], dp[N - 1][1]) << '\n';
+
+    for(int i=1; i<=2; i++){
+      for(int j=1; j<=n; j++){
+        cin>>a[i][j];
+      }
+    }
+
+    
+    d[1][1]=a[1][1];
+    d[2][1]=a[2][1];
+    d[1][2]=a[2][1]+a[1][2];
+    d[2][2]=a[1][1]+a[2][2];
+
+
+    for(int i=3; i<=n; i++){
+     d[1][i]=max({d[1][i-2],d[2][i-2],d[2][i-1]})+a[1][i];
+     d[2][i]=max({d[1][i-2],d[2][i-2],d[1][i-1]})+a[2][i];
+    }
+
+    int ans=0;
+    for(int i=1; i<=2; i++){
+      for(int j=1; j<=n; j++){
+        ans=max(ans,d[i][j]);
+      }
+    }
+
+    cout<<ans<<'\n';
   }
+  
 }
-
-/*
-dp[i][j] : i번째 열까지에서 점수의 최댓값, 단 j행 i열의 스티커는 반드시 선택
-
-가장 직전에 붙인 스티커가 i-2열의 스티커인 경우 : max(dp[i - 2][0], dp[i - 2][1]) + arr[i][j]
-가장 직전에 붙인 스티커가 i-1열의 스티커인 경우 : dp[i-1][1-j] + arr[i][j]
-
-마지막 열에 있는 스티커 중 어느 하나는 반드시 붙인게 최댓값이므로 max(dp[N - 1][0], dp[N - 1][1])을 계산하면 됨
-*/
