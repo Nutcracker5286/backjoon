f0a9397e3c6edaab4d28980315471e44a3fcd610
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-26 02:17:16 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,53 +1,52 @@
-// d[i]= i항이 마지막 항인 가장 긴 증부수의 길이
-// for(0:n) i 결정
-//  for(0:i) 테이블을 채우기 위해 탐색하는 인자
 #include <bits/stdc++.h>
 using namespace std;
+
 
 int n;
 int a[1005];
 int d[1005];
 int pre[1005];
-int arr[1005];
+
 
 int main(){
-  ios_base::sync_with_stdio(0);
+  ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin>>n;
 
-  for(int i=0; i<n; i++){
-    cin>>a[i];
-    d[i]=1;
-  }
+  fill(d,d+n,1);
+  for(int i=0; i<n; i++)
+  cin>>a[i];
 
 
   for(int i=0; i<n; i++){
     for(int j=0; j<i; j++){
-      if(a[i]>a[j]){ //i항이 마지막 항이니까
-        if(d[i]<d[j]+1){ //탐색길이가 더 길때
-          d[i]=d[j]+1; pre[i]=j;
-        }
-      }
+      if(a[i]<=a[j]) continue;
+      if(d[i]>=d[j]+1) continue;
+      d[i]=d[j]+1; pre[i]=j;
     }
   }
 
-  int ans=0; int ans_idx;
+  int mx=1;
+  int ansIdx=0;
   for(int i=0; i<n; i++){
-    if(d[i]>ans){
-      ans=d[i];
-      ans_idx=i;
-    }
+    if(mx > d[i]) continue;
+    mx=d[i]; ansIdx=i;
   }
-  cout<<ans<<'\n';
 
+  stack<int> st;
   int idx=0;
-  for(int i=ans_idx; idx<ans; i=pre[i]){
-    arr[idx++]=a[i];
+  for(int i=ansIdx; idx<mx; i=pre[i]){
+    st.push(a[i]); idx++;
   }
-  sort(arr,arr+ans);
-
-  for(int i=0; i<ans; i++)
-  cout<<arr[i]<<" ";
   
+  cout<<mx<<'\n';
+  for(int i=0; i<mx; i++)
+  {
+    cout<<st.top()<<' '; st.pop();
+  }
+
+
+
+
 }
