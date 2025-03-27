e323308ac191b2c11b335fb5a3c725fd87c58528
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-27 17:39:04 +0900

    [Silver I] Title: 포도주 시식, Time: 36 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -1,38 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+int a[10005];
+int d[10005][3];
 int n;
-long d[10005][3];
-long a[10005];
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin>>n;
-
-  for(int i=1; i<=n; ++i)
+  for(int i=1; i<=n; i++)
   cin>>a[i];
 
   d[1][1]=a[1];
   d[2][1]=a[2];
-  d[2][2]=a[1]+a[2];
-  for(int i=3; i<=n ; ++i){
-    long mx=0;
-    for(int j=1; j<=i-2; ++j){
-      for(int k=1; k<=2; k++){
-        mx=max(mx,d[j][k]);
-      }
-    }
+  d[2][2]=a[2]+a[1];
+  for(int i=3; i<=n; i++){
+    int mx=0;
+    for(int j=1; j<=i-2; j++)
+      mx=max({d[j][1],d[j][2],mx});
+
     d[i][1]=mx+a[i];
     d[i][2]=d[i-1][1]+a[i];
+
+   
   }
 
-  long ans=d[1][1];
-  for(int j=2; j<=n; ++j){
-    for(int k=1; k<=2; k++){
-      ans=max(ans,d[j][k]);
-    }
-  }
-  cout<<ans;
+  int ans=d[1][1]; 
+  for(int j=1; j<=n; j++)
+    ans=max({d[j][1],d[j][2],ans});
+    cout<<ans;
 }
