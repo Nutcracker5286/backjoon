2782ba83a4050adf3cfc5aad5672bf26245db056
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-03-27 21:09:47 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.cc
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -3,33 +3,34 @@ using namespace std;
 #define ll long long
 
 int n,m;
-
 ll d[45];
-vector<ll> vip={0};
+vector<ll> vip;
+
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin>>n>>m;
 
-  for(int i=1; i<=m; ++i){
-    int tmp;
-    cin>>tmp;
-    vip.push_back(tmp);
+  vip.push_back(0);
+  for(int i=0; i<m; i++){
+    ll t; cin>>t;
+    vip.push_back(t);
   }
-
   vip.push_back(n+1);
 
-  d[0] = 1;  // 빈 자리가 없는 경우
-  d[1]=1; 
-  d[2]=2;
+  d[0]=1;
+  d[1]=1; d[2]=2; d[3]=3;
 
-  for(int i=3; i<=n; i++){
+  for(int i=4; i<=n; i++){
     d[i]=d[i-1]+d[i-2];
   }
 
-  int ans=1;
-  for (int i = 1; i < vip.size(); i++) 
-    ans *= d[vip[i] - vip[i - 1] - 1];
+  ll ans=1;
+  for(int i=0; i<=m; i++){
+   ans*=d[vip[i+1]-vip[i]-1];
+  }
   cout<<ans;
+
+
 }
