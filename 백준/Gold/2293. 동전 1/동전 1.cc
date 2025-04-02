fc3875ad87aed2af6d80184183bb3258b37c49fd
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2025-04-02 21:56:55 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int n,k;
+int a[105];
+int d[10005];
+
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin>>n>>k;
+
+  for(int i=1; i<=n; i++){
+    cin>>a[i];
+  }
+
+  d[0]=1;
+  for(int i=1; i<=n; i++){
+    for(int j=a[i]; j<=k; j++){
+      d[j]=d[j]+d[j-a[i]];
+    }
+  }
+  cout<<d[k];
+}
