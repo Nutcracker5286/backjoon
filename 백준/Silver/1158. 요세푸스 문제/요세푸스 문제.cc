8fa7dccd9f98ca97169959b3daa786e5fb364385
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-01-17 03:41:57 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 80 ms, Memory: 2284 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,35 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    list<int> L;
-    int N, K,cnt;
-    cin >> N >> K;
-    cnt=N;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    for (int i = 1; i <= N; i++)
-    {
+    int N, K;
+    cin >> N >> K;
+
+    list<int> L;
+    for(int i = 1; i <= N; i++){
         L.push_back(i);
     }
 
     auto p = L.begin();
-    cout<<"<";
-    while (N--)
-    {
-        for (int j = 0; j < K; j++)
-        // k번째 숫자 제거
-        {
-            if (p != L.end())
-                p++;
-            else
-                {p = L.begin(); p++;}
+
+    cout << "<";
+
+    while(!L.empty()){
+        //  K-1번 이동
+        for(int i = 0; i < K-1; i++){
+            p++;
+            if(p == L.end()) 
+                p = L.begin();
         }
-        p--;
-        if(L.size()==1) cout<<*p<<">";
-        else cout<<*p<<", ";
-        
-        cnt--;
-        p=L.erase(p);
+
+        // 출력
+        cout << *p;
+        if(L.size() > 1) cout << ", ";
+
+        //  제거 → p는 자동으로 다음 위치
+        p = L.erase(p);
+
+        // 만약 erase로 end가 되었으면 begin으로 보정
+        if(p == L.end()) 
+            p = L.begin();
     }
+
+    cout << ">";
 }
