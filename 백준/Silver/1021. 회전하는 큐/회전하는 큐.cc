d8348538e9eccca939d67321894e733decb113c4
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-01-30 14:58:21 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m;
+    int cnt=0;
+    cin>>n>>m;
+
+    deque<int> s;
+
+    for (int i = 1; i <= n; i++)
+    {
+        s.push_back(i);
+    }
+    
+    while (m--)
+    {
+        int t;
+        cin>>t;
+        int idx=find(s.begin(), s.end(), t)-s.begin();
+        while (s.front()!=t)
+        {//왼쪽이동
+            if(idx<=(int)s.size()/2){
+                s.push_back(s.front());
+                s.pop_front();
+            }
+            else{
+                s.push_front(s.back());
+                s.pop_back();
+            }
+            cnt++;
+        }
+        s.pop_front();
+        
+    }
+    cout<<cnt;
+}
