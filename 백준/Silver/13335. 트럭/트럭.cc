d53473e15443a7662429e6eef98d66933a49f80d
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-02-11 03:03:19 +0900

    [Silver I] Title: 트럭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.cc
+++ 백준/Silver/13335. 트럭/트럭.cc
@@ -1,44 +1,53 @@
-/*
-큐로 대기 하는 트럭 관리
-
-다리 배열에서 한칸씩 이동하면서 시간 카운트
-
-현재 다리 무게를 계산하고 최대 하중
-보다 낮으면 한칸 이동
-무게와 대기열의 트럭 무게 합이 하중보다 작으면 다리에 싣기
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
-int road[105];
-queue<int> tr;
-int n, w, L, ans;
+/*
+w길이의 다리를 n개의 트럭이 건너야 함
+다리가 버티는 최대 하중은 L
+
+대기 트럭을 큐로 관리
+다리에 직접 트럭을 놓으면서 시간 카운트 진행
+
+대기열이 비워지고, 다리 위에트럭이 없어야 종료
+
+전진시킴
+다리 무게와 대기열 트럭 무게 합이 하중 이하면
+다리에 트럭 싣고 대기열에서 제거
+*/
+
+queue<int> q;
+int n, w, L;
+int bridge[105];
 
 bool isEmpty()
 {
     for (int i = 0; i < w; i++)
-        if (road[i] != 0)
-            return 0;
-    return 1;
-}
-
-int calc()
-{
-    int sum = 0;
-    for (int i = 0; i < w; i++)
-        sum += road[i];
-    return sum;
-}
-
-void go()
-{
-    for (int i = w - 1; i > 0; i--)
     {
-        road[i] = road[i - 1];
+        if (bridge[i] != 0)
+            return false;
     }
-    road[0] = 0;
+    return true;
 }
 
+void fw()
+{
+    // 한칸씩 전진
+    for (int i = w - 1; i >= 0; i--)
+    {
+        bridge[i + 1] = bridge[i];
+    }
+    bridge[0] = 0;
+}
+
+int total()
+{
+    int t = 0;
+    for (int i = 0; i < w; i++)
+    {
+        t += bridge[i];
+    }
+    return t;
+}
 int main()
 {
     ios::sync_with_stdio(0);
@@ -50,24 +59,23 @@ int main()
     {
         int t;
         cin >> t;
-        tr.push(t);
+        q.push(t);
     }
 
-    while (!tr.empty() || !isEmpty())
+    // 건너는 시뮬레이션 시작
+    int ans = 0;
+    // 대기열이 비워지고, 다리 위에트럭이 없어야 종료
+    while (!q.empty() || !isEmpty())
     {
-        go(); // 한칸 전진
+        fw();
         ans++;
-        // 다리 무게 계산
-        int cur = calc();
 
-        // 최대 하중이하면
-        if (cur <= L)
+        // 현재 다리위의 무게와 대기열 무게 합이 하중보다 작은지 체크
+        if (!q.empty() && total() + q.front() <= L)
         {
-            if (!tr.empty() && cur + tr.front() <= L)
-            {
-                road[0] = tr.front();
-                tr.pop();
-            }
+            // 작으면 다리에 싣기
+            bridge[0] = q.front();
+            q.pop();
         }
     }
     cout << ans;
