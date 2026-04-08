ed7b3cda40ea8b97f7579503bbca1a172d469e52
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-04-08 15:26:22 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,14 +1,13 @@
 -- 코드를 작성해주세요
 /*
-id, 이메일, 이름 ,성
-개발자 테이블에서 파이썬 또는 c#
-
-이진수 비트 연산으로 추출 
-개발자의 스킬 코드 & 서브쿼리 스킬코드에서 파이선
-c#
+파이선이나 c# 스킬을 가져야함
+아이디 이메일 이름 성 
 */
-select d.id, d.email, d.first_name,d.last_name
-from developers d
-where d.skill_code  & (select code from skillcodes where name = 'Python' )
-or d.skill_code  & (select code from skillcodes where name = 'C#' )
-order by id
+SELECT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+FROM DEVELOPERS D
+# 비트 엔드 연산시 
+WHERE D.SKILL_CODE & 
+(SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')
+OR D.SKILL_CODE & 
+(SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')
+ORDER BY ID
