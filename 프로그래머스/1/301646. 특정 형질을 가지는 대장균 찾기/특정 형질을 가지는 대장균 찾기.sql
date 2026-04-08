017b2f214d7caaf16e986921916cc88cc0ff8db7
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-04-08 15:58:31 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,11 +1,7 @@
--- 코드를 작성해주세요
 /*
-2번 형질은 없고 1번 || 3번 형질 보유 개체 수
-
-2로 비트 엔드 연산에서 나오는 값이 아니면 됨
+2번은 안되고 , 1번이나 3번 형질 보유 개체 수 출력
 */
-
-select count(id)  as count
-from ecoli_data
-where genotype & 2 = 0
-and (genotype & 3 or genotype & 4)
+SELECT COUNT(*) AS COUNT
+FROM ECOLI_DATA
+WHERE GENOTYPE & 5
+AND !(GENOTYPE & 2) 
