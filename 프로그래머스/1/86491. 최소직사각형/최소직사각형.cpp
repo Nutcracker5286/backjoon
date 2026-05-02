b8d42fcc74d6480de656c7c810c0a9e87724d413
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-05-02 21:48:40 +0900

    [level 1] Title: 최소직사각형, Time: 1.37 ms, Memory: 5.04 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -5,46 +5,22 @@ using namespace std;
 명함의 가로 세로 전환 가능
 모든 명함을 수납 가능한 가장 작은 지각 만들기
 
-가능한 길이 중 최대 길이 찾기
-그걸 가로로 설정
-나머지 좌표들을 가로 세로 중 짧은 쪽을 새 벡터에 담기
-새 백터 내림차 정렬, 제일 앞에 것 제출
+따라서 명함 중 긴쪽을 한 방향 ex) 가로로 몰기
+그러면 긴쪽에서 젤 긴 값, 짧은쪽에서 제일 긴 값만 추출
 
 
 */
-int w,h;
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0;
     
-    int ix =0, idx;
-    int mn = 0;
-    for(auto c: sizes){
-        int tmp = max(c[0],c[1]);
-        if(tmp > mn){
-            mn =tmp;
-            idx= ix;
-        }
-        ix++;
+    int maxW =0, maxH=0;
+    for(auto c : sizes){
+        int w = c[1], h =c[0];
+        
+        maxH = max(maxH, max(w,h));
+        maxW = max(maxW, min(w,h));
     }
     
-    
-    int x = sizes[idx][0] , y =sizes[idx][1];
-    int mx = max(x,y);
-    int my = min(x,y);
-    
-    // 해당 idx 제외하고 작은쪽 길이 삽입
-    vector<int> res;
-    res.push_back(my);
-    for(int i=0; i<sizes.size(); i++){
-        if(i==idx) continue;
-        res.push_back(min(sizes[i][0],sizes[i][1]));
-    }
-    sort(res.begin(),res.end(), greater<int>{});
-    answer = mx *res[0]; 
-    
-    
-    
-    
-    return answer;
+    return maxW*maxH;
 }
