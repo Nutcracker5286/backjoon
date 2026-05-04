058383201e1f8395304075a165ba9ceefd515c37
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-05-04 15:32:19 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    for(auto c: strlist)
+        answer.push_back(c.size());
+    return answer;
+}
