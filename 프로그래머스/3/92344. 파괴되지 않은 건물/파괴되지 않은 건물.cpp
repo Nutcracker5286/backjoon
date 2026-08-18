3837d1e9345226a5fb7a2f14f17ba52766c5a704
HoJunLee <144647180+Nutcracker5286@users.noreply.github.com>
2026-08-18 13:14:26 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 23.60 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.cpp
@@ -3,57 +3,53 @@
 
 using namespace std;
 
-void doSkill(vector<vector<int>> &diff, vector<int> &skill){
-    int ty;
-    ty= skill[0] == 1 ?-1:1;
-    int cx = skill[1], cy=skill[2];
-    int ex = skill[3], ey =skill[4];
-    int deg = skill[5]*ty;
-    diff[cx][cy]+=deg;
-    diff[cx][ey+1]-=deg;
-    diff[ex+1][cy]-=deg;
+// 타입 시작x,y 끝 x,y 배수
+void makeDiff(vector<int> &v, vector<vector<int>> &diff){
+    int deg = (v[0] ==1?  -v[5] : v[5]);
+    int sx = v[1], sy= v[2];
+    int ex = v[3], ey= v[4];
+    
+    diff[sx][sy] += deg;
+    diff[sx][ey+1]  -=deg;
+    diff[ex+1][sy] -=deg;
     diff[ex+1][ey+1]+=deg;
-
-    
-    
 }
 
 
-
-
-
-
-/*
-스킬을 탐색하면서 요규하는 상태 적용 시키기
-*/
 int solution(vector<vector<int>> brd, vector<vector<int>> skill) {
     int answer = 0;
-    vector<vector<int>> diff(brd.size()+1,vector<int>(brd[0].size()+1,0));
-    for(auto a : skill){
-        doSkill(diff,a);
+    
+    int n =brd.size(), m = brd[0].size();
+    
+    
+    //차분 배열 설정
+    vector<vector<int>> diff(n+1, vector<int>(m+1,0));
+    
+    for(auto v : skill){
+        makeDiff(v, diff);
     }
-        
-
-    // 가로 누적합
-    for(int i = 0; i < brd.size(); i++){
-        for(int j = 1; j < brd[0].size(); j++){
-            diff[i][j] += diff[i][j-1];
+    
+    //가로 누적합
+    for(int i=0; i<n; i++){
+        for(int j=1; j<m; j++){
+            diff[i][j]+=diff[i][j-1];
         }
     }
-
-    // 세로 누적합
-    for(int j = 0; j < brd[0].size(); j++){
-        for(int i = 1; i < brd.size(); i++){
-            diff[i][j] += diff[i-1][j];
+    
+    
+    //세로 누적합
+    for(int i=0; i<m; i++){
+        for(int j=1; j<n; j++){
+            diff[j][i]+=diff[j-1][i];
         }
     }
-
-    // 최종 검사
-    for(int i = 0; i < brd.size(); i++){
-        for(int j = 0; j < brd[0].size(); j++){
-            answer += (brd[i][j] + diff[i][j] > 0);
+    
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            answer+=(diff[i][j]+brd[i][j]>0);
         }
-    } 
+    }
     
     
     return answer;
